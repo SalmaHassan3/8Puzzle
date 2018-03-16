aaf8dff0a6ac3211097db00df7f916606f626ec3
SalmaHassan3927 <salmahassan_3927@yahoo.com>
2018-03-16 19:46:47 +0200

    A*Manhattan

--- 8Puzzle.py
+++ 8Puzzle.py
@@ -4,13 +4,16 @@ Created on Thu Mar 15 15:23:44 2018
 
 @author: salma
 """ 
+import heapq
 class Node:
     def __init__( self, board, parent, operator, depth, cost ):
         self.board= board
         self.parent = parent
         self.operator = operator
         self.depth = depth
-        self.cost = cost
+        self.cost = self.depth+self.Manhattan([1,2,3,4,5,6,7,8,0])
+        print(self.board)
+        print(self.cost)
     def checkPath(self,nodeBoard):
         temp=self
         while True:
@@ -20,12 +23,36 @@ class Node:
                 break
             temp = temp.parent
         return True
-
-
-             
+    def Manhattan(self,goal):
+        distance=0
+        for num in range(0,9):
+            index1=self.board.index(num)
+            index2=goal.index(num)
+            x1=getx(index1)
+            y1=gety(index1)
+            x2=getx(index2)
+            y2=gety(index2)
+            distance+=abs(x1-x2)+abs(y1-y2)
+        return distance
+    def __lt__(self, other):
+         return self.cost < other.cost
+   
+def getx(index):
+    if index in [0,1,2]:
+        return 0
+    elif index in [3,4,5]:
+        return 1
+    else:
+        return 2
+def gety(index):
+    if index in [0,3,6]:
+        return 0
+    elif index in [1,4,7]:
+        return 1
+    else:
+        return 2
 
 def displayBoard( state ):
-    
     print ("-------------")
     print ("| %d | %d | %d |" % (state[0], state[1], state[2])) 
     print ("-------------")
@@ -86,16 +113,17 @@ def createNode( board, parent, operator, depth, cost ):
 
 def expandNode( node ):
     children = []
-    if moveUp(node.board) != None:
-        newNode=createNode( moveUp( node.board ), node, "u", node.depth + 1, 0 )
-        if node.checkPath(newNode.board):
-            children.append(newNode)
+    
            
     if moveDown(node.board) != None:
             newNode=createNode( moveDown( node.board ), node, "d", node.depth + 1, 0 )
             if node.checkPath(newNode.board):
                 children.append(newNode)
-             
+    if moveUp(node.board) != None:
+        newNode=createNode( moveUp( node.board ), node, "u", node.depth + 1, 0 )
+        if node.checkPath(newNode.board):
+            children.append(newNode)
+         
     if moveLeft(node.board) != None:
             newNode=createNode( moveLeft( node.board ), node, "l", node.depth + 1, 0 )
             if node.checkPath(newNode.board):
@@ -115,17 +143,12 @@ def BFS(start,goal):
     while True:
         if len( frontier ) == 0:
             return None
-
         node = frontier.pop(0)
         if node.board == goal:
-            
             moves = []
             moves.insert(0, node)
-            temp = node
-           
+            temp = node         
             while True:
-                
-                 
                  if temp.parent == None: 
                      break
                  moves.insert(0, temp.parent)
@@ -133,34 +156,58 @@ def BFS(start,goal):
             return moves
         children= expandNode(node) 
         frontier.extend(children)
+"dfssssssssssss"
 def DFS(start,goal):
     frontier =[]
     frontier.append( createNode( start, None, None, 0, 0 ) )
     while True:
         if len( frontier ) == 0:
             return None
-
         node = frontier.pop()
-        if node.board == goal:
-            
+        if node.board == goal:          
             moves = []
             moves.insert(0, node)
-            temp = node
-           
+            temp = node           
             while True:
-                
-                 
                  if temp.parent == None: 
                      break
                  moves.insert(0, temp.parent)
                  temp = temp.parent
             return moves
         children= expandNode(node) 
-        frontier.extend(children)   
+        frontier.extend(children) 
+
+def A_star(start,goal):
+    frontier=[]
+    start=createNode(start, None, None, 0, 0 )
+    heapq.heappush(frontier,start)
+    
+    while True:
+        
+        if len( frontier ) == 0:
+            return None
+        node=heapq.heappop(frontier)
+        if node.board == goal:          
+            moves = []
+            moves.insert(0, node)
+            temp = node           
+            while True:
+                 if temp.parent == None: 
+                     break
+                 moves.insert(0, temp.parent)
+                 temp = temp.parent
+            return moves
+        children= expandNode(node)
+        for num in range(0,len(children)):
+            heapq.heappush(frontier,children[num])
+          
+     
+    
+       
 
 def main():
-    startState=[1,2,5,3,4,0,6,7,8]
-    goal = [0,1,2,3,4,5,6,7,8]
+    startState=[4,1,3,0,2,6,7,5,8]
+    goal = [1,2,3,4,5,6,7,8,0]
 #    root = createNode( startState , None , None , 0, 0 )
 #    children1 = expandNode(root)
 #    print ("1")
@@ -177,7 +224,7 @@ def main():
 #    print ("3") 
 #    for num in range(0,len(children3)): 
 #     print (children3[num].board)
-    moves = BFS(startState,goal)
+    moves = A_star(startState,goal)
     print("moves are")
     for num in range(0,len(moves)):
          displayBoard(moves[num].board)
