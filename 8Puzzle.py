d9b5ecce1586ab3d0d914e70104dddb8a391364e
SalmaHassan3927 <salmahassan_3927@yahoo.com>
2018-03-16 16:21:01 +0200

    duplicates fixed

--- 8Puzzle.py
+++ 8Puzzle.py
@@ -11,19 +11,27 @@ class Node:
         self.operator = operator
         self.depth = depth
         self.cost = cost
-		
-		
-		
-		
+    def checkPath(self,nodeBoard):
+        temp=self
+        while True:
+            if temp.board==nodeBoard:
+                return False
+            if temp.parent == None:
+                break
+            temp = temp.parent
+        return True
+
+
+             
 
 def displayBoard( state ):
     
     print ("-------------")
-    print ("| %i | %i | %i |") % (state[0], state[1], state[2])
+    print ("| %d | %d | %d |" % (state[0], state[1], state[2])) 
     print ("-------------")
-    print ("| %i | %i | %i |") % (state[3], state[4], state[5])
+    print ("| %d | %d | %d |" % (state[3], state[4], state[5])) 
     print ("-------------")
-    print ("| %i | %i | %i |") % (state[6], state[7], state[8])
+    print ("| %d | %d | %d |" % (state[6], state[7], state[8])) 
     print ("-------------")       
         
 "moveeeeeee"
@@ -79,22 +87,32 @@ def createNode( board, parent, operator, depth, cost ):
 def expandNode( node ):
     children = []
     if moveUp(node.board) != None:
-            children.append( createNode( moveUp( node.board ), node, "u", node.depth + 1, 0 ) )
+        newNode=createNode( moveUp( node.board ), node, "u", node.depth + 1, 0 )
+        if node.checkPath(newNode.board):
+            children.append(newNode)
+           
     if moveDown(node.board) != None:
-            children.append( createNode( moveDown( node.board ), node, "d", node.depth + 1, 0 ) )
+            newNode=createNode( moveDown( node.board ), node, "d", node.depth + 1, 0 )
+            if node.checkPath(newNode.board):
+                children.append(newNode)
+             
     if moveLeft(node.board) != None:
-            children.append( createNode( moveLeft( node.board ), node, "l", node.depth + 1, 0 ) )
+            newNode=createNode( moveLeft( node.board ), node, "l", node.depth + 1, 0 )
+            if node.checkPath(newNode.board):
+                 children.append(newNode)
+              
     if moveRight(node.board) != None:
-            children.append( createNode( moveRight( node.board), node, "r", node.depth + 1, 0 ) )
+            newNode=createNode( moveRight( node.board), node, "r", node.depth + 1, 0 )
+            if node.checkPath(newNode.board):
+                children.append(newNode)
     return children
+             
 
 "bfsssssssssss"
 def BFS(start,goal):
     frontier =[]
-    
     frontier.append( createNode( start, None, None, 0, 0 ) )
     while True:
-#        print "salma"
         if len( frontier ) == 0:
             return None
 
@@ -113,45 +131,55 @@ def BFS(start,goal):
                  moves.insert(0, temp.parent)
                  temp = temp.parent
             return moves
-        print ("new nela")
         children= expandNode(node) 
-        print ("parent")
-        displayBoard(node.board)
-        print ("children")
-        for num in range(0,len(children)):
-           
-           displayBoard(children[num].board)
-         
         frontier.extend(children)
-    
+def DFS(start,goal):
+    frontier =[]
+    frontier.append( createNode( start, None, None, 0, 0 ) )
+    while True:
+        if len( frontier ) == 0:
+            return None
+
+        node = frontier.pop()
+        if node.board == goal:
+            
+            moves = []
+            moves.insert(0, node)
+            temp = node
+           
+            while True:
+                
+                 
+                 if temp.parent == None: 
+                     break
+                 moves.insert(0, temp.parent)
+                 temp = temp.parent
+            return moves
+        children= expandNode(node) 
+        frontier.extend(children)   
 
 def main():
-    startState=[1,2,3,6,0,7,8,5,4]
+    startState=[1,2,5,3,4,0,6,7,8]
     goal = [0,1,2,3,4,5,6,7,8]
-    root = createNode( startState , None , None , 0, 0 )
-    children1 = expandNode(root)
-    print ("1")
-    for num in range(0,len(children1)): 
-     print (children1[num].board)
-    children2 = expandNode(children1[0])
-    print ("2")
-    
-    
-    for num in range(0,len(children2)):
-       
-     print (children2[num].board)
-    children3 = expandNode(children1[1])
-    print ("3") 
-    for num in range(0,len(children3)): 
-     print (children3[num].board)
-  #  moves = BFS(startState,goal)
+#    root = createNode( startState , None , None , 0, 0 )
+#    children1 = expandNode(root)
+#    print ("1")
+#    for num in range(0,len(children1)): 
+#     print (children1[num].board)
+#    children2 = expandNode(children1[0])
+#    print ("2")
 #    
-#    for num in range(0,len(moves)):
-#         
-#         displayBoard(moves[num].board)
-         
-     
-    
-       
+#    
+#    for num in range(0,len(children2)):
+#       
+#     print (children2[num].board)
+#    children3 = expandNode(children1[1])
+#    print ("3") 
+#    for num in range(0,len(children3)): 
+#     print (children3[num].board)
+    moves = BFS(startState,goal)
+    print("moves are")
+    for num in range(0,len(moves)):
+         displayBoard(moves[num].board)
 if __name__ == "__main__":
 	main()    
