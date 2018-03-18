dc92605384464e7fee16182ed44c6b08cf51650a
SalmaHassan3927 <salmahassan_3927@yahoo.com>
2018-03-18 15:16:58 +0200

    final

--- 8Puzzle.py
+++ 8Puzzle.py
@@ -7,7 +7,9 @@ Created on Thu Mar 15 15:23:44 2018
 import heapq
 import math
 import timeit
+#A class for nodes in search tree
 class Node:
+#constructor
     def __init__( self, board, parent, operator, depth):
         self.board= board
         self.parent = parent
@@ -15,6 +17,7 @@ class Node:
         self.depth = depth
         self.manhattan_cost = self.depth+self.Manhattan([0,1,2,3,4,5,6,7,8])
         self.euclidean_cost= self.depth+self.Euclidean([0,1,2,3,4,5,6,7,8])
+#A function to check if a given node(board) is a duplicate in this node's path 
     def checkPath(self,nodeBoard):
         temp=self
         while True:
@@ -24,6 +27,7 @@ class Node:
                 break
             temp = temp.parent
         return True
+#A function to calculate Manhattan distance from goal
     def Manhattan(self,goal):
         distance=0
         for num in range(0,9):
@@ -35,6 +39,7 @@ class Node:
             y2=gety(index2)
             distance+=abs(x1-x2)+abs(y1-y2)
         return distance
+#A function to calculate Euclidean distance from goal
     def Euclidean(self,goal):
         distance=0
         for num in range(0,9):
@@ -46,10 +51,12 @@ class Node:
             y2=gety(index2)
             distance+=math.sqrt((x1-x2)**2+(y1-y2)**2)
         return distance
-#    def __lt__(self, other):
-#         return self.manhattan_cost < other.manhattan_cost
+#Less than function to compare nodes according to their costs(used by heap in A* search)
     def __lt__(self, other):
-         return self.euclidean_cost < other.euclidean_cost
+         return self.manhattan_cost < other.manhattan_cost
+#    def __lt__(self, other):
+#         return self.euclidean_cost < other.euclidean_cost
+#Takes index of element in board reprsented by 1-D array and returns its X co-ordinate in 2-D board         
 def getx(index):
     if index in [0,1,2]:
         return 0
@@ -57,6 +64,7 @@ def getx(index):
         return 1
     else:
         return 2
+#Takes index of element in board reprsented by 1-D array and returns its Y co-ordinate in 2-D board 
 def gety(index):
     if index in [0,3,6]:
         return 0
@@ -73,8 +81,8 @@ def displayBoard( state ):
     print ("-------------")
     print ("| %d | %d | %d |" % (state[6], state[7], state[8])) 
     print ("-------------")       
-        
-"moveeeeeee"
+       
+#Move functions in four directions
 def moveLeft( board ):
     indexZero = board.index( 0 )
     if indexZero not in [0, 3, 6]:
@@ -119,41 +127,37 @@ def moveDown( board ):
     else:
         return None   
     
-"buildddddddd"
+
 
 def createNode( board, parent, operator, depth):
 	return Node( board, parent, operator, depth)
 
 def expandNode( node ):
     children = []
-    
-           
-    if moveDown(node.board) != None:
-            newNode=createNode( moveDown( node.board ), node, "d", node.depth + 1  )
-            if node.checkPath(newNode.board):
-                children.append(newNode)
-    if moveUp(node.board) != None:
-        newNode=createNode( moveUp( node.board ), node, "u", node.depth + 1)
-        if node.checkPath(newNode.board):
-            children.append(newNode)
-         
-    if moveLeft(node.board) != None:
-            newNode=createNode( moveLeft( node.board ), node, "l", node.depth + 1)
-            if node.checkPath(newNode.board):
-                 children.append(newNode)
-              
     if moveRight(node.board) != None:
             newNode=createNode( moveRight( node.board), node, "r", node.depth + 1)
             if node.checkPath(newNode.board):
                 children.append(newNode)
+    if moveDown(node.board) != None:
+            newNode=createNode( moveDown( node.board ), node, "d", node.depth + 1  )
+            if node.checkPath(newNode.board):
+                children.append(newNode)
+    if moveLeft(node.board) != None:
+            newNode=createNode( moveLeft( node.board ), node, "l", node.depth + 1)
+            if node.checkPath(newNode.board):
+                 children.append(newNode)  
+    if moveUp(node.board) != None:
+        newNode=createNode( moveUp( node.board ), node, "u", node.depth + 1)
+        if node.checkPath(newNode.board):
+            children.append(newNode)
     return children
              
 nodes_Expanded=0
-"bfsssssssssss"
+#BFS search
 def BFS(start,goal):
     global nodes_Expanded
     frontier =[]
-    frontier.append( createNode( start, None, None, 0) )
+    frontier.append( createNode( start, None, None, 0))
     while True:
         if len( frontier ) == 0:
             return None
@@ -168,10 +172,11 @@ def BFS(start,goal):
                  moves.insert(0, temp.parent)
                  temp = temp.parent
             return moves
-        nodes_Expanded=nodes_Expanded+1
+        
         children= expandNode(node) 
+        nodes_Expanded=nodes_Expanded+len(children)
         frontier.extend(children)
-"dfssssssssssss"
+#DFS search
 def DFS(start,goal):
     global nodes_Expanded
     frontier =[]
@@ -190,10 +195,10 @@ def DFS(start,goal):
                  moves.insert(0, temp.parent)
                  temp = temp.parent
             return moves
-        nodes_Expanded=nodes_Expanded+1
-        children= expandNode(node) 
+        children= expandNode(node)
+        nodes_Expanded=nodes_Expanded+len(children)
         frontier.extend(children) 
-
+#A* search
 def A_star(start,goal):
     global nodes_Expanded
     frontier=[]
@@ -215,17 +220,14 @@ def A_star(start,goal):
                  moves.insert(0, temp.parent)
                  temp = temp.parent
             return moves
-        nodes_Expanded=nodes_Expanded+1
         children= expandNode(node)
+        nodes_Expanded=nodes_Expanded+len(children)
         for num in range(0,len(children)):
             heapq.heappush(frontier,children[num])
-          
-     
-    
-       
-
+ 
 def main(): 
     startState=[]
+    start=stop=0
     print('Enter start state element by element:')
     for i in range(9):
         x = int(input('-->'))
@@ -237,22 +239,20 @@ def main():
         start = timeit.default_timer()
         moves = BFS(startState,goal)
         stop = timeit.default_timer()
-        print ("Running time is: %d s" %(stop - start ))
     if method==2:  
         start = timeit.default_timer()
         moves = DFS(startState,goal)
         stop = timeit.default_timer()
-        print ("Running time is: %d s" %(stop - start ))
     if method==3:
         start = timeit.default_timer()
         moves = A_star(startState,goal)
         stop = timeit.default_timer()
-        print ("Running time is: %d s" %(stop - start ))
     print("Moves are:")
     for num in range(0,len(moves)):
          displayBoard(moves[num].board)
     print("Cost of path is: %d" %moves[len(moves)-1].depth) 
     print("Depth of path is: %d" %moves[len(moves)-1].depth)
-    print("Number of nodes expanded is: %d "%nodes_Expanded)
+    print("Number of nodes expanded is: %d "%(nodes_Expanded+1))
+    print ("Running time is: %d s" %(stop - start ))
 if __name__ == "__main__":
 	main()    
